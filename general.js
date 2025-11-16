383be3e43f28bf3bacce7c100c8e9012f7b2bea3
brianpr-pr <brian23perezperez>
2025-11-16 20:27:16 +0000

    Added function for button to hide or show the nav

--- general.js
+++ general.js
@@ -7,4 +7,25 @@ document.addEventListener("DOMContentLoaded", () => {
             div.style.transition = "transform 0.1s linear";
         });
     });
+    
+    document.getElementById('boton-hamburguesa').addEventListener("click", (event) =>{
+        if(window.innerWidth >= 768){
+            cambiarEstadoNav();
+        }
+    })
 });
+
+function cambiarEstadoNav(){
+    let nav = document.getElementById('navegable');
+    let main = document.getElementById('main');
+    let arrClasesNav = Array.from(nav.classList);
+
+    if(arrClasesNav.includes("d-md-flex")){
+        console.log("It works");
+        nav.classList.remove("d-md-flex");
+        main.classList.remove("col-md-8");
+    } else{
+        nav.classList.add("d-md-flex");
+        main.classList.add("col-md-8");
+    }
+}
